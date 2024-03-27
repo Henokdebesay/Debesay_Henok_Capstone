1de7386d91860b97aa93ca6fb2a7887967535a6a
Henok Debesay <henokdebesay@gmail.com>
2024-03-27 02:32:05 -0700

    working on marker image

--- client/src/Pages/EnglandMap.jsx
+++ client/src/Pages/EnglandMap.jsx
@@ -34,47 +34,45 @@ function EnglandMap() {
     });
 
     map.on('load', () => {
-      // Add the custom marker image
       map.loadImage('../soccer.png', (error, image) => {
         if (error) throw error;
         map.addImage('../soccer.png', image);
         
-        // Add markers for each team
-        teams.forEach(team => {
-          const [longitude, latitude] = team.city;
-          const popupContent = `
-            <div>
-              <h3>${team.name}</h3>
-              <p><strong>Founded:</strong> ${team.founded}</p>
-              <p><strong>Stadium:</strong> ${team.stadium}</p>
-              <p><strong>Nickname:</strong> ${team.nickname}</p>
-              <p><strong>League:</strong> ${team.league}</p>
-            </div>
-          `;
-
-          // Create the custom marker
-          const marker = new mapboxgl.Marker({ 
-            element: createCustomMarkerElement('client/public/soccer.png') // Pass the image id
-          })
-            .setLngLat([longitude, latitude])
-            .setPopup(new mapboxgl.Popup().setHTML(popupContent))
-            .addTo(map);
-        });
+teams.forEach(team => {
+    const [longitude, latitude] = team.city;
+    const popupContent = `
+      <div>
+        <h3>${team.name}</h3>
+        <p><strong>Founded:</strong> ${team.founded}</p>
+        <p><strong>Stadium:</strong> ${team.stadium}</p>
+        <p><strong>Nickname:</strong> ${team.nickname}</p>
+        <p><strong>League:</strong> ${team.league}</p>
+      </div>
+    `;
+  
+    new mapboxgl.Marker({ 
+      element: createCustomMarkerElement('soccer.png') 
+    })
+    .setLngLat([longitude, latitude])
+    .setPopup(new mapboxgl.Popup().setHTML(popupContent))
+    .addTo(map);
+  });
+  
       });
     });
 
     return () => map.remove();
   }, [teams]);
 
-  // Helper function to create custom marker element
-  const createCustomMarkerElement = (markerId) => {
+const createCustomMarkerElement = (markerId) => {
     const markerElement = document.createElement('div');
     markerElement.className = 'custom-marker';
     markerElement.style.width = '30px';
     markerElement.style.height = '30px';
-    markerElement.style.backgroundImage = `url(../soccer.png)`; // URL to your marker image
+    markerElement.style.backgroundImage = `url(${markerId})`;
     return markerElement;
   };
+  
 
   return <div id="map-container" style={{ width: '1000px', height: '820px' }} />;
 }
